98c2aee06a71f3c79e6e24d36f416c7e24f9b1a9
Kyle Forkey <kyleforkey@ethmint.io>
2022-06-23 13:02:24 -0500

    dfghjkl

--- api/runPython/index.py
+++ api/runPython/index.py
@@ -1,10 +1,11 @@
 from http.server import BaseHTTPRequestHandler
 from cowpy import cow
+import json
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        
+        self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         """ egg = input("Enter your name: ")
@@ -12,13 +13,8 @@ class handler(BaseHTTPRequestHandler):
         if input() == "kyle":
             print("fuck you")  """
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
-        response =  'Just doin tests'
-        #response = message.milk('Hello from Python from a Serverless Function!')
-        self.wfile.write(bytes(response, 'utf-8'))
-
+        self.wfile.write(message.encode())
         
-       
-        
-        return
+        return json.dumps(message)
 
     
