241fada4c2433cfc9f6f89534ae55c49643af590
Kyle Forkey <kyleforkey@ethmint.io>
2022-06-23 12:44:37 -0500

    tests

--- api/runPython/index.py
+++ api/runPython/index.py
@@ -3,10 +3,8 @@ from cowpy import cow
 
 class handler(BaseHTTPRequestHandler):
 
-#get the request message from the client
-
     def do_GET(self):
-        #request = self.path
+        
         self.send_header('Content-type','text/plain')
         self.end_headers()
         """ egg = input("Enter your name: ")
@@ -14,8 +12,9 @@ class handler(BaseHTTPRequestHandler):
         if input() == "kyle":
             print("fuck you")  """
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
-        self.send_response(200,message)
-        
+        response =  self.wfile.write(message.encode()).decode()
+        self.send_response(200, response)
+       
         
         return
 
