328c2c831acf2cd717b7627cdaa0cf4fa71c3d43
Kyle Forkey <kyleforkey@ethmint.io>
2022-06-23 13:10:37 -0500

    dfb

--- api/runPython/index.py
+++ api/runPython/index.py
@@ -5,7 +5,7 @@ import json
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200, 'ola')
+        self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         """ egg = input("Enter your name: ")
