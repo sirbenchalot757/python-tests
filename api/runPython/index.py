af77eeb642a83b54de8b2d63c2174e488474beab
Kyle Forkey <kyleforkey@ethmint.io>
2022-06-30 11:20:49 -0500

    dfg

--- api/runPython/index.py
+++ api/runPython/index.py
@@ -1,12 +1,15 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
    def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-
-        message = "Hello, World!"
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
         self.wfile.write(bytes(message, "utf8"))
-
-    
--- pythonServer.py
+++ pythonServer.py
@@ -1,13 +1,20 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-
-        message = "Hello, World!"
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
         self.wfile.write(bytes(message, "utf8"))
+
+    
 
 with HTTPServer(('', 8000), handler) as server:
     server.serve_forever()
